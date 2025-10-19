39b73a18bab57026071d75f29c45f6202a82c4f4
sunyang <sunyang20040908@163.com>
2025-10-19 15:24:35 +0800

    完成详情页搭建

---
+++ src/apis/detail.js
@@ -0,0 +1,21 @@
+import httpInstance from "@/utils/http";
+
+export function getDetailAPI(id){
+    return httpInstance({
+        url:'/goods',
+        params:{
+            id
+        }
+    })
+}
+
+export const getHotGoodsAPI = ({ id, type, limit = 3 }) => {
+    return httpInstance({
+        url:'/goods/hot',
+        params:{
+            id, 
+            type, 
+            limit
+        }
+    })
+}
--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import App from './App.vue'
 import router from './router'
 import { imageEmits } from 'element-plus'
 import { lazyPludin } from './directives'
+import { componentPlugin } from './components'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPludin)
+app.use(componentPlugin)
 
 app.mount('#app')
